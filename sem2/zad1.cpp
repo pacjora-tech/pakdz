e9a525f136c9848c4dbc30b5857355db8f31bc0b
pacjora-tech <pac.jora@gmail.com>
2025-12-22 13:09:10 +0300

    qw3e

--- sem2/zad1.cpp
+++ sem2/zad1.cpp
@@ -14,20 +14,4 @@ int main(){
     L=0.5*(p*(v*v)*s*c);
     cout<<"Value of lift power is: " << L << endl;
     return 0;
-}#include <iostream>
-using namespace std;
-
-int main(){
-    double s,v,p,c,L;
-    cout<<"Input sqare of a wing:\n";
-    cin>>s;
-    cout<<"Input velocity:\n";
-    cin>>v;
-    cout<<"Input dencity of air:\n";
-    cin>>p;
-    cout<<"Input value of lift coefficient:\n";
-    cin>>c;
-    L=0.5*(p*(v*v)*s*c);
-    cout<<"Value of lift power is: " << L << endl;
-    return 0;
 }
